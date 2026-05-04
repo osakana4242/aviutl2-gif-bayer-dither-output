42f82a5c5f04f85c534e44ab0f899daca91e9574
osakana4242 <osakana4242@gmail.com>
2026-05-04 11:34:31 +0900

    feat: avi で webセーフベイヤーディザ出力成功

--- vs/Project/GifSaver.cpp
+++ vs/Project/GifSaver.cpp
@@ -1,6 +1,8 @@
 //----------------------------------------------------------------------------------
 //	サンプルAVI(vfw経由)出力プラグイン for AviUtl ExEdit2
 //----------------------------------------------------------------------------------
+
+#include <algorithm>
 #include <windows.h>
 #include <vfw.h>
 #pragma comment(lib, "vfw32.lib")
@@ -23,6 +25,57 @@ OUTPUT_PLUGIN_TABLE output_plugin_table = {
 	func_config,									// 出力設定のダイアログを要求された時に呼ばれる関数へのポインタ (nullptrなら呼ばれません)
 	func_get_config_text,							// 出力設定のテキスト情報を取得する時に呼ばれる関数へのポインタ (nullptrなら呼ばれません)
 };
+
+
+// 8x8 Bayer
+static const int bayer8x8[8][8] = {
+    { 0,48,12,60,3,51,15,63 },
+    {32,16,44,28,35,19,47,31},
+    { 8,56, 4,52,11,59, 7,55},
+    {40,24,36,20,43,27,39,23},
+    { 2,50,14,62, 1,49,13,61},
+    {34,18,46,30,33,17,45,29},
+    {10,58, 6,54, 9,57, 5,53},
+    {42,26,38,22,41,25,37,21}
+};
+
+inline unsigned char quantize_websafe(int v, int x, int y) {
+    double d = (bayer8x8[y & 7][x & 7] / 64.0) - 0.5;
+    double val = v + d * 51.0;
+
+    val = std::clamp(val, 0.0, 255.0);
+    int level = (int)(val / 51.0 + 0.5);
+    level = std::clamp(level, 0, 5);
+
+    return (unsigned char)(level * 51);
+}
+
+void process_frame(uint8_t* data, int w, int h) {
+    // BI_RGB は下から上に並んでる
+    int stride = w * 3;
+
+    for (int y = 0; y < h; y++) {
+        int yy = h - 1 - y; // 上下反転
+        uint8_t* row = data + yy * stride;
+
+        for (int x = 0; x < w; x++) {
+            uint8_t* px = row + x * 3;
+
+            // BGR順
+            uint8_t b = px[0];
+            uint8_t g = px[1];
+            uint8_t r = px[2];
+
+            r = quantize_websafe(r, x, y);
+            g = quantize_websafe(g, x, y);
+            b = quantize_websafe(b, x, y);
+
+            px[0] = b;
+            px[1] = g;
+            px[2] = r;
+        }
+    }
+}
 
 //---------------------------------------------------------------------
 //	プラグインDLL初期化関数 (未定義なら呼ばれません)
@@ -120,6 +173,10 @@ bool func_output(OUTPUT_INFO* oip) {
 
 		// ビデオの書き込み
 		void* data = oip->func_get_video(frame, BI_RGB);
+	
+	   // ★ここで加工
+    process_frame((uint8_t*)data, oip->w, oip->h);
+
 		if (AVIStreamWrite(avi.pvideo, frame, 1, data, bmih.biSizeImage, AVIIF_KEYFRAME, NULL, NULL) != S_OK) {
 			break;
 		}
